dba3802f4b5662e2f37f383ea7011e60b15ca465
PandaTrumpet <dsavontrumpet@gmail.com>
2024-09-13 22:20:19 +0300

    update

--- vite-project/src/components/Review/Review.jsx
+++ vite-project/src/components/Review/Review.jsx
@@ -2,12 +2,24 @@ import css from "./Review.module.css";
 
 export default function Review({ review }) {
   const firstLetter = review.reviewer_name.charAt(0);
+  const rating = review.reviewer_rating;
+  const stars = Array(review.reviewer_rating).fill(0);
+  console.log(stars);
+
   return (
     <div>
       <div>
         <p>{firstLetter}</p>
+        <div>
+          <ul>
+            {stars.map((_, index) => (
+              <span key={index}>★</span>
+            ))}
+          </ul>
+          <h3>{review.reviewer_name}</h3>
+        </div>
       </div>
-      <h3>{review.reviewer_name}</h3>
+      <p>{review.comment}</p>
     </div>
   );
 }
