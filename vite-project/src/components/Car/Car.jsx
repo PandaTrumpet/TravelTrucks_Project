aa594db89c0c0aaa0162d42bd6032eb57084de7c
PandaTrumpet <dsavontrumpet@gmail.com>
2024-09-13 15:37:16 +0300

    update

--- vite-project/src/components/Car/Car.jsx
+++ vite-project/src/components/Car/Car.jsx
@@ -1,4 +1,25 @@
 import css from "./Car.module.css";
-export default function Car() {
-  return <div></div>;
+import sprite from "../../images/sprite.svg";
+export default function Car({ car }) {
+  console.log(car);
+
+  const picture = car.gallery[0].thumb;
+  return (
+    <div className={css.container}>
+      <div>
+        <img src={picture} alt="" className={css.picture} />
+      </div>
+      <div className={css.infoContainer}>
+        <div className={css.titleContainer}>
+          <h3 className={css.name}>{car.name}</h3>
+          <div className={css.priceContainer}>
+            <p className={css.price}>€{car.price}</p>
+            <svg className={css.iconHeart}>
+              <use href={`${sprite}#icon-heart`}></use>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
