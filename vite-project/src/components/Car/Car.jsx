618e2423ac41727b9dc0c1c388b4f72120bc117c
PandaTrumpet <dsavontrumpet@gmail.com>
2024-09-13 17:22:16 +0300

    updtae

--- vite-project/src/components/Car/Car.jsx
+++ vite-project/src/components/Car/Car.jsx
@@ -3,23 +3,61 @@ import sprite from "../../images/sprite.svg";
 export default function Car({ car }) {
   console.log(car);
 
-  const picture = car.gallery[0].thumb;
+  const picture = car;
+
   return (
     <div className={css.container}>
-      <div>
-        <img src={picture} alt="" className={css.picture} />
-      </div>
+      <div
+        className={css.picture}
+        style={{ backgroundImage: `url(${picture.gallery[0].original})` }}
+      ></div>
       <div className={css.infoContainer}>
         <div className={css.titleContainer}>
           <h3 className={css.name}>{car.name}</h3>
-          <div className={css.priceContainer}>
-            <p className={css.price}>€{car.price}</p>
-            <svg className={css.iconHeart}>
-              <use href={`${sprite}#icon-heart`}></use>
-            </svg>
+          <div className={css.mainInfo}>
+            <div className={css.priceContainer}>
+              <p className={css.price}>€{car.price}</p>
+              <svg className={css.iconHeart}>
+                <use href={`${sprite}#icon-heart`}></use>
+              </svg>
+            </div>
           </div>
         </div>
+        <div className={css.detalis}>
+          <svg className={css.iconStar}>
+            <use href={`${sprite}#icon-star`}></use>
+          </svg>
+          <p>{car.rating}</p>
+          <p>({car.reviews.length} Reviews)</p>
+          <svg className={css.iconMap}>
+            <use href={`${sprite}#icon-map`}></use>
+          </svg>
+          <p>{car.location}</p>
+        </div>
+        <p className={css.description}>
+          {car.description.length > 64
+            ? car.description.slice(0, 64) + "..."
+            : car.description}
+        </p>
+        {/* <ul>
+          <li>
+            <svg>
+              <use></use>
+            </svg>
+            <p></p>
+          </li>
+          <li>
+            <svg>
+              <use></use>
+            </svg>
+            <p></p>
+          </li>
+        </ul> */}
       </div>
     </div>
   );
+}
+
+{
+  /* <img src={picture.gallery[0].thumb} alt="" className={css.picture} /> */
 }
