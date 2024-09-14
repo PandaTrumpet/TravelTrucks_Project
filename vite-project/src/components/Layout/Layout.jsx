8bb5b13760299d8f032f9bb2576a2daa2de2c587
PandaTrumpet <dsavontrumpet@gmail.com>
2024-09-14 22:33:42 +0300

    update

--- vite-project/src/components/Layout/Layout.jsx
+++ vite-project/src/components/Layout/Layout.jsx
@@ -1,9 +1,10 @@
 import Navigation from "../Navigation/Navigation.jsx";
 // eslint-disable-next-line react/prop-types
+import css from "./Layout.module.css";
 export default function Layout({ children }) {
   return (
     <>
-      <header>
+      <header className={css.nav}>
         <Navigation />
       </header>
       {children}
