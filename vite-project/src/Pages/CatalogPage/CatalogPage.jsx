225faf9bcb1a9c99a602ab86f5379ae5f6111702
PandaTrumpet <dsavontrumpet@gmail.com>
2024-09-14 22:24:58 +0300

    update

--- vite-project/src/Pages/CatalogPage/CatalogPage.jsx
+++ vite-project/src/Pages/CatalogPage/CatalogPage.jsx
@@ -3,7 +3,7 @@ import Sidebar from "../../components/Sidebar/Sidebar.jsx";
 import { useDispatch } from "react-redux";
 import { fetchCars } from "../../Redux/cars/operation.js";
 import CarsList from "../../components/CarsList/CarsList.jsx";
-
+import css from "./CatalogPage.module.css";
 export default function CatalogPage() {
   const dispatch = useDispatch();
 
@@ -12,9 +12,9 @@ export default function CatalogPage() {
   }, [dispatch]);
 
   return (
-    <>
+    <div className={css.container}>
       <Sidebar />
       <CarsList />
-    </>
+    </div>
   );
 }
