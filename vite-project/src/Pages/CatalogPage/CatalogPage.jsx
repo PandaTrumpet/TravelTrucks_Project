3be17297ae6d506446b28ac0d84ea4156d2e8914
PandaTrumpet <dsavontrumpet@gmail.com>
2025-01-03 22:37:17 +0100

    update

--- vite-project/src/Pages/CatalogPage/CatalogPage.jsx
+++ vite-project/src/Pages/CatalogPage/CatalogPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import Sidebar from "../../components/Sidebar/Sidebar.jsx";
 import { useDispatch } from "react-redux";
-import { fetchCars } from "../../Redux/cars/operation.js";
+import { fetchCars } from "../../redux/cars/operation.js";
 import CarsList from "../../components/CarsList/CarsList.jsx";
 import css from "./CatalogPage.module.css";
 
