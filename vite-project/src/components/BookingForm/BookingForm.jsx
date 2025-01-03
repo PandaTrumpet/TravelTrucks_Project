8c409718a46c2536ab266a29ac66f65d50590514
PandaTrumpet <dsavontrumpet@gmail.com>
2025-01-03 20:37:32 +0100

    update

--- vite-project/src/components/BookingForm/BookingForm.jsx
+++ vite-project/src/components/BookingForm/BookingForm.jsx
@@ -1,45 +1,24 @@
-// import { useId } from "react";
-// import css from "./BookingForm.module.css";
-// import { Formik, Form, Field } from "formik";
-// export default function BookingForm() {
-//   const nameId = useId();
-//   const emailId = useId();
-//   const textareaId = useId();
-//   const dateId = useId();
-//   const initialValues = { name: " ", email: "", message: "", date: "" };
-//   const handleSubmit = (values, action) => {
-//     console.log(values);
-//     action.resetForm();
-//   };
-//   return (
-//     <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-//       <Form>
-//         <label htmlFor={nameId}>
-//           <Field type="text" name="name" id={nameId} placeholder="Name" />
-//         </label>
-//         <label htmlFor={emailId}>
-//           <Field type="text" name="email" id={emailId} />
-//         </label>
-//         <label htmlFor={dateId}>
-//           <Field type="date" id={dateId} name="date" />
-//         </label>
-//         <label htmlFor={textareaId}>
-//           <Field
-//             className={css.textarea}
-//             component="textarea"
-//             id={textareaId}
-//             name="message"
-//           />
-//         </label>
-//         <button type="submit">Send</button>
-//       </Form>
-//     </Formik>
-//   );
-// }
-
 import { useId } from "react";
 import css from "./BookingForm.module.css";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import toast, { Toaster } from "react-hot-toast";
+
+const validationSchema = Yup.object({
+  name: Yup.string().required("Name is required"),
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  date: Yup.string()
+    .matches(
+      /^\d{4}-\d{2}-\d{2} - \d{4}-\d{2}-\d{2}$/,
+      "Date must be in format YYYY-MM-DD - YYYY-MM-DD"
+    )
+    .required("Date is required"),
+  message: Yup.string(),
+});
+
+const initialValues = { name: "", email: "", message: "", date: "" };
 
 export default function BookingForm() {
   const nameId = useId();
@@ -47,11 +26,10 @@ export default function BookingForm() {
   const textareaId = useId();
   const dateId = useId();
 
-  const initialValues = { name: "", email: "", message: "", date: "" };
-
-  const handleSubmit = (values, action) => {
+  const handleSubmit = (values, actions) => {
     console.log(values);
-    action.resetForm();
+    toast.success("Your request has been sent!");
+    actions.resetForm();
   };
 
   return (
@@ -60,49 +38,63 @@ export default function BookingForm() {
       <p className={css.formText}>
         Stay connected! We are always ready to help you.
       </p>
-      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-        <Form className={css.form}>
-          <label htmlFor={nameId}>
-            <Field
-              type="text"
-              name="name"
-              id={nameId}
-              placeholder="Name"
-              className={css.input}
-            />
-          </label>
-          <label htmlFor={emailId}>
-            <Field
-              type="text"
-              name="email"
-              id={emailId}
-              placeholder="Enter your email"
-              className={css.input}
-            />
-          </label>
-          <label htmlFor={dateId}>
-            <Field
-              type="text"
-              id={dateId}
-              name="date"
-              placeholder="Booking date*"
-              className={css.input}
-            />
-          </label>
-          <label htmlFor={textareaId}>
-            <Field
-              className={css.textarea}
-              component="textarea"
-              id={textareaId}
-              name="message"
-              placeholder="Comment" // Плейсхолдер для текстового поля
-            />
-          </label>
-          <button type="submit" className={css.btn}>
-            Send
-          </button>
-        </Form>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {() => (
+          <Form className={css.form}>
+            <label htmlFor={nameId}>
+              <Field
+                type="text"
+                name="name"
+                id={nameId}
+                placeholder="Name"
+                className={css.input}
+              />
+              <ErrorMessage name="name" component="div" className={css.error} />
+            </label>
+            <label htmlFor={emailId}>
+              <Field
+                type="text"
+                name="email"
+                id={emailId}
+                placeholder="Enter your email"
+                className={css.input}
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className={css.error}
+              />
+            </label>
+            <label htmlFor={dateId}>
+              <Field
+                type="text"
+                id={dateId}
+                name="date"
+                placeholder="Booking date* "
+                className={css.input}
+              />
+              <ErrorMessage name="date" component="div" className={css.error} />
+            </label>
+            <label htmlFor={textareaId}>
+              <Field
+                className={css.textarea}
+                component="textarea"
+                id={textareaId}
+                name="message"
+                placeholder="Comment"
+              />
+            </label>
+            <button type="submit" className={css.btn}>
+              Send
+            </button>
+          </Form>
+        )}
       </Formik>
+      <Toaster />
     </div>
   );
 }
